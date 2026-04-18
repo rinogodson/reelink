5680067275d791e4cc6a7994cac6aa8d1d568da5
Rino Godson <rinogodson@gmail.com>
2026-04-18 21:49:09 +0530

    astro's config is the real culprit????

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,15 @@ import tailwindcss from "@tailwindcss/vite";
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
+  site: "https://reelink.rinogodson.workers.dev", // Anchors your exact domain!
   output: "server",
-  adapter: cloudflare(), // Swapped Vercel for Cloudflare
+  adapter: cloudflare(),
+  trailingSlash: "ignore", // Safely moved out of vite, prevents Cloudflare conflicts
+
   vite: {
     optimizeDeps: {
       exclude: ["better-auth"],
     },
-    trailingSlash: "never",
     ssr: {
       external: ["node:async_hooks", "node:events", "node:util"],
     },
